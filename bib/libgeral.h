04782aa0cdd2ac2424d3675fad5b5bd158f874ce
ana mel <99690268+anamels@users.noreply.github.com>
2024-05-12 22:00:16 -0400

    Update libgeral.h

--- bib/libgeral.h
+++ bib/libgeral.h
@@ -1,27 +1,26 @@
 #ifndef __LIBGERAL__
 #define __LIBGERAL__
 
-// hash
 typedef struct {
     int codigo_ibge, capital, codigo_uf, siafi_id, ddd;
     double latitude, longitude; 
     char nome[50], fuso_horario[20];
-} tcidade;
+} tmunicipio;
 
 typedef struct {
-    tcidade *vetor;
+    tmunicipio *vetor;
     int qtdElementos;
     int tamanho_max;
-} thash; 
+} thash;
 
 int hash1(int chave, int seed, int tamanho_hash);
 int hash2(int chave, int tamanho_hash);
-int inserirHash(thash *hash, tcidade bucket);
+int inserirHash(thash *hash, tmunicipio bucket);
 int construirHash(thash *hash);
-tcidade *buscarHash(thash hash, int chave);
+tmunicipio *buscarHash(thash hash, int chave);
 void deletarHash(thash *hash);
 
-// abb
+
 typedef struct {
     int codigo_ibge;
     double latitude, longitude;
@@ -45,21 +44,20 @@ typedef struct {
 typedef struct {
     tvizinho *vizinhos;
     int tamanho_max;
-    int qtde_elementos;
+    int qtdElementos;
 } theap;
 
 int inserirArvore(ttree *arvore, tnode **atual, telementoNo node, int nivel);
-tvizinho vizinhosProximos(tnode **atual, tcidade *cidade, int nivel, theap *heap);
+tvizinho vizinhosProximos(tnode **atual, tmunicipio *municipio, int nivel, theap *heap);
 void construirArvore(ttree *arvore);
 
-// heap
+
 int inicializaHeap(theap *heap, int n);
 tvizinho acessa_max(tvizinho vetor[]);
 void altera_prioridade(tvizinho vetor[], theap heap, int n, tvizinho novoVizinho);
 int insere_elemento(tvizinho vetor[], theap *heap, tvizinho novoVizinho);
 void heap_sort(tvizinho vetor[], theap heap);
 
-// geral
 int lerArquivo(const char *caminhoArquivo, thash *hash, ttree *arvore);
 
 #endif
